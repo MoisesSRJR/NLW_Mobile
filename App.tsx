d48a9e5801aa9e451b40df34ef9226eda5ca7cb9
MoisesSRJR <moises.sampaiojr@outlook.com>
2022-10-21 01:18:28 -0300

    add new components and adjust home

--- App.tsx
+++ App.tsx
@@ -11,6 +11,7 @@ import {
 } from '@expo-google-fonts/inter';
 
 export default function App() {
+  
   const [fontsLoaded] = useFonts({
     Inter_400Regular,
     Inter_600SemiBold,
---
+++ src/utils/games.ts
@@ -0,0 +1,26 @@
+export const GAMES = [
+    {
+        id: '1',
+        name: 'League of Legends',
+        ads: '1',
+        cover: require('../../assets/Group.png')
+    },
+    {
+        id: '2',
+        name: 'League of Legends 2',
+        ads: '2',
+        cover: require('../../assets/Group.png')
+    },
+    {
+        id: '3',
+        name: 'League of Legends 3',
+        ads: '3',
+        cover: require('../../assets/Group.png')
+    },
+    {
+        id: '4',
+        name: 'League of Legends 4',
+        ads: '4',
+        cover: require('../../assets/Group.png')
+    },
+]
